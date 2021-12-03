888f2ce9df3c7c8d6df839d9d4531765bac31108
m02242a <baharford@gmail.com>
2021-12-03 16:49:40 -0500

    added mixin.  mixins can be used to reduce code duplication

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -58,6 +58,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -82,6 +83,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ? 'sale-border' : '';
