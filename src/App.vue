9efa33d6941b3374b336d92c0c54fcd858593682
m02242a <baharford@gmail.com>
2021-12-08 16:55:37 -0500

    create child components for robot builder

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,21 @@
     </nav>
   </header>
   <main>
-    <!-- <RobotBuilder /> -->
-    <Search />
+    <RobotBuilder />
+    <!-- Search / -->
   </main>
 </template>
 
 <script>
 // import HomePage from './home/HomePage.vue';
-// import RobotBuilder from './build/RobotBuilder.vue';
-import Search from './search/Search.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
+// import Search from './search/Search.vue';
 
 export default {
   name: 'App',
   components: {
-    // RobotBuilder,
-    Search,
+    RobotBuilder,
+    // Search,
   },
 };
 </script>
