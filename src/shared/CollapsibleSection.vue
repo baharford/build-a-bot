e1db8446b56a4fdd1580cbaf58756528aed8687c
m02242a <baharford@gmail.com>
2021-12-08 18:48:28 -0500

    added code to inject content into a child component with slots

---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="header">
+            <span c-if="open" @click="open = !open">&#x25b2; Collapse</span>
+            <span c-if="!open" @click="open = !open">&#x25bC; Expand</span>
+        </div>
+        <!-- Toggleable Content -->
+        <slot v-if="open">
+          <div>DefaultContent</div>
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return { open: true };
+  },
+};
+</script>
+
+<style scoped>
+  .header {
+      background-color: #bbb;
+      padding: 3px;
+      cursor: pointer;
+  }
+</style>
