1f42a9ad08a4ee8e7b6e42f3dd59777ba34f96b7
m02242a <baharford@gmail.com>
2021-12-10 08:28:06 -0500

    updated code to link to routed pages

--- src/home/HomePage.vue
+++ src/home/HomePage.vue
@@ -4,7 +4,7 @@
       <img class="robot" src="../assets/robot-home.png" aria-hidden="true" />
     </div>
     <div class="get-started">
-      <a href="">Getting started</a> building your first robot!
+      <router-link to="/build">Getting started</router-link> building your first robot!
     </div>
   </div>
 </template>
