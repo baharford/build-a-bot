f39e5101818f72d9dd8d77c3e4b2e8af999db481
m02242a <baharford@gmail.com>
2021-12-15 18:07:50 -0500

    updated code to use nested routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,11 @@ import {
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import BrowseHeads from '../parts/RobotHeads.vue';
+import BrowseArms from '../parts/RobotArms.vue';
+import BrowseTorsos from '../parts/RobotTorsos.vue';
+import BrowseBases from '../parts/RobotBases.vue';
 
 export default createRouter({
   history: createWebHashHistory(),
@@ -18,6 +23,33 @@ export default createRouter({
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
+  },
+  {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: BrowseHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: BrowseArms,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: BrowseTorsos,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: BrowseBases,
+      },
+    ],
   },
   {
     path: '/parts/:partType/:id',
