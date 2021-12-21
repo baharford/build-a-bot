7cd233ac81d8f8ddf203217310c6e0339554af53
m02242a <baharford@gmail.com>
2021-12-21 17:06:48 -0500

    organized the store with modules

--- src/parts/get-parts-mixin.js
+++ src/parts/get-parts-mixin.js
@@ -4,12 +4,17 @@ export default {
   },
   computed: {
     parts() {
-      return this.$store.state.parts || {
+      return this.$store.state.robots.parts || {
         heads: [],
         arms: [],
         torsos: [],
         bases: [],
       };
+    },
+  },
+  getters: {
+    cartSaleItems(state) {
+      return state.cart.filter((item) => item.head.onSale);
     },
   },
 };
