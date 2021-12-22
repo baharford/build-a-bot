2af7a6a648fcd6a482e38fa47ac81a5f5f0b5c32
m02242a <baharford@gmail.com>
2021-12-22 17:38:58 -0500

    updated code to pass data to directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,10 +1,11 @@
 export default {
-  beforeMount(element) {
+  beforeMount(element, binding) {
+    if (binding.arg !== 'position') return;
+    Object.keys(binding.modifiers).forEach((key) => {
+      // eslint-disable-next-line no-param-reassign
+      element.style[key] = '5px';
+    });
     // eslint-disable-next-line no-param-reassign
     element.style.position = 'absolute';
-    // eslint-disable-next-line no-param-reassign
-    element.style.bottom = '5px';
-    // eslint-disable-next-line no-param-reassign
-    element.style.right = '5px';
   },
 };
