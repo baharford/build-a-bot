24f9399354f492a0c4bf88f1e52772b8311594d2
m02242a <baharford@gmail.com>
2021-12-22 17:46:59 -0500

    updated code to use directive lifecycle hooks

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,11 +1,9 @@
-export default {
-  beforeMount(element, binding) {
-    if (binding.arg !== 'position') return;
-    Object.keys(binding.modifiers).forEach((key) => {
-      // eslint-disable-next-line no-param-reassign
-      element.style[key] = '5px';
-    });
+export default function (element, binding) {
+  if (binding.arg !== 'position') return;
+  Object.keys(binding.modifiers).forEach((key) => {
     // eslint-disable-next-line no-param-reassign
-    element.style.position = 'absolute';
-  },
-};
+    element.style[key] = '5px';
+  });
+  // eslint-disable-next-line no-param-reassign
+  element.style.position = 'absolute';
+}
